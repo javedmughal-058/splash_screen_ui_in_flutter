8a55ea85717f1aefd6ebd7eff6378c549dcac6b8
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2022-10-06 17:15:56 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# splash_screen_ui_in_flutter
+ This is only splash screen design for flutter projects
---
+++ android/app/src/main/kotlin/com/example/splash_screen_ui_in_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.splash_screen_ui_in_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
