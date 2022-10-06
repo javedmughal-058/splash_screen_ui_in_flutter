5ae884833e618f5030ab37172a03c0f5bf8736d6
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2022-10-06 18:13:19 +0500

    modify

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:splash_screen_ui_in_flutter/main.dart';
+import 'package:splash_screen_ui_in_flutter/splashScreen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget( AnimatedSplashScreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
